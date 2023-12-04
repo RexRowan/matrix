9d34d4d13ead02f6501d4e8bba441e227d290f4f
MeanMachineRex <RexRowan@users.noreply.github.com>
2023-12-04 09:02:50 +0000

    icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 
-const TerminalModal = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const handleClose = () => setIsOpen(false);
+const TerminalModal = ({ isOpen, handleClose }) => {
+  TerminalModal.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    handleClose: PropTypes.func.isRequired,
+  };
 
   if (!isOpen) return null;
 
@@ -23,13 +25,31 @@ const TerminalModal = () => {
   );
 };
 
+const TerminalIcon = ({ handleOpen }) => {
+  return (
+    <div className="terminal-icon" onClick={handleOpen}>
+      <img src="/src/assets/kali.png" alt="Terminal Icon" />
+    </div>
+  );
+};
+
+TerminalIcon.propTypes = {
+  handleOpen: PropTypes.func.isRequired,
+};
+
 const HomePage = () => {
+  const [isTerminalOpen, setIsTerminalOpen] = useState(false);
+
+  const openTerminal = () => setIsTerminalOpen(true);
+  const closeTerminal = () => setIsTerminalOpen(false);
+
   return (
     <div className="homepage">
-      <TerminalModal />
+      <TerminalModal isOpen={isTerminalOpen} handleClose={closeTerminal} />
+      {!isTerminalOpen && <TerminalIcon handleOpen={openTerminal} />}
       {/* Rest of the homepage content */}
     </div>
   );
 };
 
-export default HomePage;
+export default HomePage
